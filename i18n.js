633e7c618dd693606a5060f4c59794c4fc93ba77
IsdbSscDev <sscdeveloper@isdb.org>
2022-06-08 14:45:49 +0530

    first commit

---
+++ i18n.js
@@ -0,0 +1,15 @@
+// const path = require('path');
+
+module.exports = {
+  i18n: {
+    defaultLocale: "ar",
+    locales: ["en", "ar"],
+    localeDetection: false,
+    //, "ar"
+    // localeSubpaths: {
+    //   ar: "ar",
+    // },
+    // localeSubpaths: true
+  },
+  //   localePath: path.resolve('public/static/locales'),
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import { appWithTranslation } from "next-i18next";
+import nextI18NextConfig from "../i18n";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default appWithTranslation(MyApp, nextI18NextConfig);
+
